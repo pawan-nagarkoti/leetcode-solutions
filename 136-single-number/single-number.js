fd912f1f18f6a16799e86687ab255b3ced827787
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-08 08:26:14 +0530

    Time: 1 ms (73.61%) | Memory: 56.5 MB (53.26%) - LeetSync

---
+++ 136-single-number/single-number.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+    // let map = {};
+
+    // let i = 0;
+    // while (i < nums.length) {
+    //     if (map[nums[i]] === undefined) {
+    //         map[nums[i]] = 1
+    //     } else {
+    //         map[nums[i]] += 1
+    //     }
+    //     ++i
+    // }
+
+    // let minKey = Infinity;
+    // let keyName;
+    // for(let key in map){
+    //     if(map[key] < minKey){
+    //         minKey = map[key];
+    //         keyName = key
+    //     }
+    // }
+   
+    // return Number(keyName)
+
+
+
+    // a ^ a = 1
+    // a ^ 0 = a
+    let i=0;
+    let xor = 0;
+    while(i<nums.length){
+        xor = xor ^ nums[i]
+        ++i
+    }
+    return xor;
+};

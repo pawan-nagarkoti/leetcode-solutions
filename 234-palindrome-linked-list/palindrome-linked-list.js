a0d3b5683e5abf92f72dec09d45fb0865ddda353
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-09 09:17:19 +0530

    Time: 227 ms (5.03%) | Memory: 83.9 MB (20.24%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.js
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -9,43 +9,83 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+// var isPalindrome = function (head) {
+
+//     let length = 0;
+//     let mainHead = head;
+//     let curr = head
+//     while (curr) {
+//         curr = curr.next
+//         ++length
+//     }
+
+
+//     let half = Math.floor(length / 2);
+//     let curr1 = head;
+//     let i = 0;
+//     while (i < half) {
+//         curr1 = curr1.next;
+//         ++i
+//     }
+
+//     let prev = null
+//     while (curr1) {
+//         let temp = curr1.next;
+//         curr1.next = prev;
+//         prev = curr1;
+//         curr1 = temp
+//     }
+//     head = prev
+
+//     let check = false;
+//     while (mainHead && head) {
+//         if (mainHead.val === head.val) {
+//             mainHead = mainHead.next;
+//             head = head.next
+//             check = true;
+//         }else{
+//              check=false
+//              break;
+//         }
+//     }
+//     return check;
+// };
+
+
 var isPalindrome = function (head) {
-    let length = 0;
-    let mainHead = head;
-    let curr = head
+    // find mid node
+    let slow = fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    // reverse
+    let prev = null;
+    let curr = slow;
+
     while (curr) {
-        curr = curr.next
-        ++length
-    }
-    console.log('l', length)
-
-    let half = Math.floor(length / 2);
-    let curr1 = head;
-    let i = 0;
-    while (i < half) {
-        curr1 = curr1.next;
-        ++i
+        let temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp;
     }
 
-    let prev = null
-    while (curr1) {
-        let temp = curr1.next;
-        curr1.next = prev;
-        prev = curr1;
-        curr1 = temp
-    }
-    head = prev
+    // check is it pailendrome
+    let mainHead = head;
+    head = prev;
 
     let check = false;
     while (mainHead && head) {
+        console.log('hii')
         if (mainHead.val === head.val) {
-            mainHead = mainHead.next;
-            head = head.next
             check = true;
-        }else{
-             check=false
-             break;
+            mainHead = mainHead.next;
+            head = head.next;
+        } else {
+            check = false;
+            break;
         }
     }
     return check;
-};
+}

f45102742d397b2da785111462c782f3d8a9b87a
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-09 10:17:06 +0530

    Time: 58 ms (89.49%) | Memory: 66 MB (85.00%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -11,21 +11,75 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
-var getIntersectionNode = function(headA, headB) {
-    let store = new Set();
-    while(headA){
-        store.add(headA);
-        headA= headA.next;
-    }
+// var getIntersectionNode = function(headA, headB) {
+//     let store = new Set();
+//     while(headA){
+//         store.add(headA);
+//         headA= headA.next;
+//     }
 
-    while(headB){
-        if(!store.has(headB)){
-            headB = headB.next
-        }else{
-            return headB
+//     while(headB){
+//         if(!store.has(headB)){
+//             headB = headB.next
+//         }else{
+//             return headB
+//         }
+//     }
+
+//     return null
+
+// };
+
+
+var getIntersectionNode = function (headA, headB) {
+    let p1 = headA;
+    let p2 = headB;
+    let check1 = false;
+    let check2 = false;
+
+
+    while (p1 !== null || p2 !== null) {
+        if (p1 == p2) {
+            return p1 || p2
         }
+        if (check1 && p1 === null || check2 && p2 === null) {
+            return null;
+        }
+        
+
+        if (p1 === null) {
+            p1 = headB
+            check1 = true;
+        } else {
+            p1 = p1.next;
+        }
+
+
+        if (p2 === null) {
+            p2 = headA
+            check2 = true;
+
+        } else {
+            p2 = p2.next;
+        }
+
     }
+}
 
-    return null
 
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

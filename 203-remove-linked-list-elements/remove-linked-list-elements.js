96de2841e2226fe88b04b0c792b620dd80dd8cfb
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-10 08:43:48 +0530

    Time: 2 ms (18.98%) | Memory: 61.6 MB (53.42%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.js
+++ 203-remove-linked-list-elements/remove-linked-list-elements.js
@@ -12,9 +12,8 @@
  */
 var removeElements = function(head, val) {
     let sentinalNode = new ListNode();
-
-    let prev = sentinalNode;
     sentinalNode.next = head;
+    let prev = sentinalNode;
     let curr = head;
 
     while(curr){
@@ -31,3 +30,20 @@ var removeElements = function(head, val) {
     return sentinalNode.next
 
 };
+
+
+var removeElements = function(head, val) {
+    let sentinel = new ListNode();
+    sentinel.next = head;
+    let prev = sentinel;
+
+    while(prev && prev.next){
+        if(prev.next.val === val){
+            prev.next = prev.next.next
+        }else{
+            prev = prev.next;
+        }
+    }
+
+    return sentinel.next
+}

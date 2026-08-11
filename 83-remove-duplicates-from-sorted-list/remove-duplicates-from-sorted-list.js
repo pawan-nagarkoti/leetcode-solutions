50ae24656248b8bf957195f12fb174151d14c2e0
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-11 08:43:29 +0530

    Time: 0 ms (100.00%) | Memory: 56.4 MB (93.87%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -28,3 +28,17 @@ var deleteDuplicates = function (head) {
     prev.next = curr
     return sentinel.next
 };
+
+
+var deleteDuplicates = function (head) {
+    let curr = head;
+    while(curr && curr.next){
+        if(curr.val === curr.next.val){
+            curr.next = curr.next.next
+        }else{
+            curr = curr.next
+        }
+    }
+
+    return head
+}

62006f18fadced71f614736ee4f6ddc0509dd835
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-11 10:52:01 +0530

    Time: 4 ms (33.60%) | Memory: 61.8 MB (37.81%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+var addTwoNumbers = function (l1, l2) {
+    let sentinel = new ListNode();
+    let prev = sentinel;
+    let carry = 0;
+
+    while (l1 || l2) {
+        l1Val = l1 ? l1.val : 0;
+        l2Val = l2 ? l2.val : 0;
+
+        let sum = l1Val + l2Val + carry;
+        carry = Math.floor(sum / 10);
+        newVal = sum % 10;
+
+        prev.next = new ListNode(newVal);
+        prev = prev.next
+
+        if (l1) {
+            l1 = l1.next
+        }
+        if (l2) {
+            l2 = l2.next
+        }
+    }
+
+    if (carry > 0) {
+        prev.next = new ListNode(carry);
+        prev = prev.next
+    }
+
+    return sentinel.next;
+};

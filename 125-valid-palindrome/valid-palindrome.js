7446cd1c20e22d7e37c01624924000184d45c834
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-15 09:55:22 +0530

    Time: 286 ms (5.41%) | Memory: 66.7 MB (5.21%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    const number = ['0','1','2','3','4','5','6','7','8','9']
+
+    const allLetters = [...lowercase,...uppercase];
+
+    let str = '';
+    let i=0;
+
+    while(i<s.length){
+        console.log(typeof s[i])
+        if(allLetters.includes(s[i])){
+            str+=s[i].toLowerCase()
+        }
+        // }else if(typeof Number(s[i]) === 'number'){
+        else if(number.includes(s[i])){
+            str+=s[i]
+        }
+        ++i
+    }
+
+    let x=0;
+    let k=str.length-1;
+    console.log(str)
+    while(x<k){
+        if(str[x] === str[k]){
+            ++x;
+            --k
+        }else{
+            return false;
+        }
+    }
+
+    return true
+
+};

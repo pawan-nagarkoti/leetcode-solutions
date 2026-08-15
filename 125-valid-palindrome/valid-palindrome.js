2a1d63cabe0adb13e36985f4595e0f9a81de78df
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-15 09:56:20 +0530

    Time: 228 ms (5.41%) | Memory: 67 MB (5.21%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -9,37 +9,34 @@ var isPalindrome = function (s) {
     const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-    const number = ['0','1','2','3','4','5','6','7','8','9']
+    const number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-    const allLetters = [...lowercase,...uppercase];
+    const allLetters = [...lowercase, ...uppercase];
 
     let str = '';
-    let i=0;
+    let i = 0;
 
-    while(i<s.length){
+    while (i < s.length) {
         console.log(typeof s[i])
-        if(allLetters.includes(s[i])){
-            str+=s[i].toLowerCase()
+        if (allLetters.includes(s[i])) {
+            str += s[i].toLowerCase()
         }
-        // }else if(typeof Number(s[i]) === 'number'){
-        else if(number.includes(s[i])){
-            str+=s[i]
+        else if (number.includes(s[i])) {
+            str += s[i]
         }
         ++i
     }
 
-    let x=0;
-    let k=str.length-1;
+    let x = 0;
+    let k = str.length - 1;
     console.log(str)
-    while(x<k){
-        if(str[x] === str[k]){
+    while (x < k) {
+        if (str[x] === str[k]) {
             ++x;
             --k
-        }else{
+        } else {
             return false;
         }
     }
-
     return true
-
 };

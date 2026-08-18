203dd81cf60b10d8b1ec49bd6772742604f78c3f
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-18 06:15:26 +0530

    Time: 1 ms (64.41%) | Memory: 53.5 MB (80.79%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minAddToMakeValid = function (s) {
+    let stack = []
+    check = false;
+    let i = 0;
+    while (i < s.length) {
+        if (stack[stack.length - 1] === '(' && s[i] === ')') {
+            stack.pop();
+        }else{
+            stack.push(s[i])
+        }
+        ++i
+    }
+
+    let count = 0;
+    while (stack.length) {
+        ++count;
+        stack.pop()
+    }
+    return count;
+};

5683ff48b696e089f85a7043b13d2774a188da4a
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-18 07:11:21 +0530

    Time: 4 ms (25.26%) | Memory: 56.4 MB (36.91%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -15,3 +15,40 @@ var reverseWords = function (s) {
 
     return newSt.join(' ')
 }
+
+
+var reverseWords = function (s) {
+    let st = [];
+    let k=0;
+    let p = ''
+    let check= false
+
+    // create custom split method
+    while(k<s.length){
+        if(s[k] !== ' '){
+            p+=s[k]
+            check = true
+        }
+        if(check && s[k] == ' '){
+            st.push(p)
+            p=''
+            check = false
+        }
+        ++k
+    }
+
+    if(p !== ''){
+        st.push(p)
+    }
+
+    let newSt = []
+    let i = st.length - 1;
+    while (i >= 0) {
+        if (st[i] !== '') {
+            newSt.push(st[i]);
+        }
+        --i
+    }
+
+    return newSt.join(' ')
+}

7180408b8a78ab0aba49143429868306cd7f2b72
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-18 10:26:49 +0530

    Time: 2205 ms (5.39%) | Memory: 74.9 MB (5.88%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.js
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.js
@@ -2,36 +2,36 @@
  * @param {string} s
  * @return {number}
  */
-
-function countF(freq) {
-    let max = -Infinity;
-    let min = +Infinity;
-
-    for (let c = 0; c < 26; c++) {
-        if (freq[c] > 0) {
-            if (freq[c] > max) max = freq[c];
-            if (freq[c] < min) min = freq[c];
-        }
-    }
-
-    return max - min;
-}
-
 var beautySum = function (s) {
-    let count = 0;
-    let i = 0;
+    let total = 0;
 
-    while (i < s.length) {
-        let map = new Array(26).fill(0); 
-        let j = i;
+    for (let i = 0; i < s.length; i++) {
 
-        while (j < s.length) {
-            map[s.charCodeAt(j) - 97] += 1; 
-            count += countF(map);  
-            ++j;
+        let freq = {}; 
+        for (let j = i; j < s.length; j++) {
+
+            let ch = s[j]; 
+            if (!freq[ch]) {
+                freq[ch] = 1;
+            } else {
+                freq[ch] += 1;
+            }
+
+            let max = -Infinity;
+            let min = Infinity;
+
+            for (let key in freq) {
+                if (freq[key] > max) {
+                    max = freq[key];
+                }
+                if (freq[key] < min) {
+                    min = freq[key];
+                }
+            }
+
+            total += max - min;
         }
-        ++i;
     }
 
-    return count;
+    return total;
 };

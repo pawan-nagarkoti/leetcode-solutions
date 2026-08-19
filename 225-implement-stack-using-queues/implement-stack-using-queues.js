ea74d28cd5c188baea93c734a981366b78fab068
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-19 09:14:45 +0530

    Time: 0 ms (100.00%) | Memory: 52.9 MB (89.66%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.js
+++ 225-implement-stack-using-queues/implement-stack-using-queues.js
@@ -1,7 +1,78 @@
+//  // using two queue
+// var MyStack = function () {
+//     this.q1 = []
+//     this.q2 = []
+// };
+
+// /** 
+//  * @param {number} x
+//  * @return {void}
+//  */
+// MyStack.prototype.push = function (x) {
+//     this.q1.push(x)
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MyStack.prototype.pop = function () {
+//     let n = this.q1.length;
+//     let i = 0;
+//     while (i < n - 1) {
+//         this.q2.push(this.q1.shift())
+//         ++i
+//     }
+//     let popElm = this.q1.shift()
+
+//     let temp = this.q1;
+//     this.q1 = this.q2;
+//     this.q2 = temp;
+
+//     return popElm;
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MyStack.prototype.top = function () {
+//     let n = this.q1.length;
+//     let i = 0;
+//     while (i < n - 1) {
+//         this.q2.push(this.q1.shift())
+//         ++i
+//     }
+//     let topElm = this.q1[0];
+//     this.q2.push(this.q1.shift())
+
+//     let temp = this.q1;
+//     this.q1 = this.q2;
+//     this.q2 = temp;
+
+//     return topElm;
+// };
+
+// /**
+//  * @return {boolean}
+//  */
+// MyStack.prototype.empty = function () {
+//     return this.q1.length === 0
+// };
+
+// /** 
+//  * Your MyStack object will be instantiated and called as such:
+//  * var obj = new MyStack()
+//  * obj.push(x)
+//  * var param_2 = obj.pop()
+//  * var param_3 = obj.top()
+//  * var param_4 = obj.empty()
+//  */
+
+
+
+
 
 var MyStack = function () {
-    this.q1 = []
-    this.q2 = []
+    this.q = []
 };
 
 /** 
@@ -9,60 +80,41 @@ var MyStack = function () {
  * @return {void}
  */
 MyStack.prototype.push = function (x) {
-    this.q1.push(x)
+    this.q.push(x)
 };
 
 /**
  * @return {number}
  */
 MyStack.prototype.pop = function () {
-    let n = this.q1.length;
+    let n = this.q.length;
     let i = 0;
     while (i < n - 1) {
-        this.q2.push(this.q1.shift())
+        this.q.push(this.q.shift())
         ++i
     }
-    let popElm = this.q1.shift()
-
-    let temp = this.q1;
-    this.q1 = this.q2;
-    this.q2 = temp;
-
-    return popElm;
+    let popElm = this.q.shift()
+    return popElm
 };
 
 /**
  * @return {number}
  */
 MyStack.prototype.top = function () {
-    let n = this.q1.length;
+    let n = this.q.length;
     let i = 0;
     while (i < n - 1) {
-        this.q2.push(this.q1.shift())
+        this.q.push(this.q.shift())
         ++i
     }
-    let topElm = this.q1[0];
-    this.q2.push(this.q1.shift())
-
-    let temp = this.q1;
-    this.q1 = this.q2;
-    this.q2 = temp;
-
-    return topElm;
+    let popElm =  this.q[0]
+    this.q.push(this.q.shift())
+    return popElm
 };
 
 /**
  * @return {boolean}
  */
 MyStack.prototype.empty = function () {
-    return this.q1.length === 0
+    return this.q.length === 0
 };
-
-/** 
- * Your MyStack object will be instantiated and called as such:
- * var obj = new MyStack()
- * obj.push(x)
- * var param_2 = obj.pop()
- * var param_3 = obj.top()
- * var param_4 = obj.empty()
- */

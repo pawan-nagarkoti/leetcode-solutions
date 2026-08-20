3c3ced1abdf365c5fcce4e661fcbf072caee95e6
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-20 09:46:29 +0530

    Time: 111 ms (22.67%) | Memory: 89 MB (12.77%) - LeetSync

--- 155-min-stack/min-stack.js
+++ 155-min-stack/min-stack.js
@@ -1,4 +1,54 @@
+// // If i am using shift and unshit then time complexity is o(n)
+// var MinStack = function () {
+//     this.stack = []
+// };
 
+// /** 
+//  * @param {number} value
+//  * @return {void}
+//  */
+// MinStack.prototype.push = function (value) {
+
+//     if (this.stack.length === 0) {
+//          this.stack.unshift([value,value])
+//     } else {
+//         let min = this.stack[0][1] < value ? this.stack[0][1] : value
+//         this.stack.unshift([value,min])
+//     }
+// };
+
+// /**
+//  * @return {void}
+//  */
+// MinStack.prototype.pop = function () {
+//     this.stack.shift()
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MinStack.prototype.top = function () {
+//     return this.stack[0][0]
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MinStack.prototype.getMin = function () {
+//     return this.stack[0][1]
+// };
+
+// /** 
+//  * Your MinStack object will be instantiated and called as such:
+//  * var obj = new MinStack()
+//  * obj.push(value)
+//  * obj.pop()
+//  * var param_3 = obj.top()
+//  * var param_4 = obj.getMin()
+//  */
+
+
+// time complexity 0(1)
 var MinStack = function () {
     this.stack = []
 };
@@ -8,12 +58,12 @@ var MinStack = function () {
  * @return {void}
  */
 MinStack.prototype.push = function (value) {
-   
     if (this.stack.length === 0) {
-         this.stack.unshift([value,value])
+        this.stack.push([value, value])
     } else {
-        let min = this.stack[0][1] < value ? this.stack[0][1] : value
-        this.stack.unshift([value,min])
+        let min = this.stack[this.stack.length - 1][1] < value ? this.stack[this.stack.length - 1][1] : value
+
+        this.stack.push([value, min])
     }
 };
 
@@ -21,21 +71,21 @@ MinStack.prototype.push = function (value) {
  * @return {void}
  */
 MinStack.prototype.pop = function () {
-    this.stack.shift()
+    this.stack.pop(this.stack[this.stack.length-1])
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function () {
-    return this.stack[0][0]
+    return this.stack[this.stack.length-1][0]
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function () {
-    return this.stack[0][1]
+    return this.stack[this.stack.length-1][1]
 };
 
 /** 

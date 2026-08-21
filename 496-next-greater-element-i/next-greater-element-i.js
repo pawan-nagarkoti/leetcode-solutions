26ac41f45dcdcc17326474e19215e2b2020b90c4
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-21 10:15:45 +0530

    Time: 0 ms (100.00%) | Memory: 56.4 MB (82.67%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -1,63 +1,94 @@
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
- */
-var nextGreaterElement = function (nums1, nums2) {
-    if (nums2.length === 0) {
-        return []
-    }
-    let n = nums2.length - 1;
+// /**
+//  * @param {number[]} nums1
+//  * @param {number[]} nums2
+//  * @return {number[]}
+//  */
+// var nextGreaterElement = function (nums1, nums2) {
+//     if (nums2.length === 0) {
+//         return []
+//     }
+//     let n = nums2.length - 1;
 
+//     let stack = []
+//     stack.push(nums2[n]);
+
+//     let map = {}
+//     map[nums2[n]] = -1
+
+//     let i = n
+//     while (i >= 0) {
+//         let top = stack[stack.length - 1]
+//         if (nums2[i] < top) {
+//             map[nums2[i]] = top;
+//             stack.push(nums2[i])
+//         } else if (nums2[i] > top) {
+//             // let check = true;
+//             // while (stack.length) {
+//             //     if (nums2[i] > stack[stack.length - 1]) {
+//             //         stack.pop()
+//             //     } else {
+//             //         map[nums2[i]] = stack[stack.length - 1];
+//             //         check = false;
+//             //         stack.push(nums2[i])
+//             //         break;
+//             //     }
+//             // }
+
+//             // if (check) {
+//             //     map[nums2[i]] = -1;
+//             //     stack.push(nums2[i])
+//             // }
+
+//             while (nums2[i] > stack[stack.length - 1] && stack.length) {
+//                 stack.pop()
+//             }
+
+//             let isTop = stack[stack.length - 1] ? stack[stack.length - 1] : -1
+//             map[nums2[i]] = isTop;
+//             stack.push(nums2[i])
+//         }
+//         --i
+//     }
+
+//     console.log(map)
+
+//     let ans = []
+//     let j = 0;
+//     while (j < nums1.length) {
+//         ans.push(map[nums1[j]])
+//         ++j
+//     }
+
+//     return ans
+// };
+
+
+var nextGreaterElement = function (nums1, nums2) {
+    let n = nums2.length - 1;
     let stack = []
     stack.push(nums2[n]);
 
     let map = {}
     map[nums2[n]] = -1
 
-    let i = n
+    let i = n - 1
     while (i >= 0) {
-        let top = stack[stack.length - 1]
-        if (nums2[i] < top) {
-            map[nums2[i]] = top;
-            stack.push(nums2[i])
-        } else if (nums2[i] > top) {
-            // let check = true;
-            // while (stack.length) {
-            //     if (nums2[i] > stack[stack.length - 1]) {
-            //         stack.pop()
-            //     } else {
-            //         map[nums2[i]] = stack[stack.length - 1];
-            //         check = false;
-            //         stack.push(nums2[i])
-            //         break;
-            //     }
-            // }
-
-            // if (check) {
-            //     map[nums2[i]] = -1;
-            //     stack.push(nums2[i])
-            // }
-
-            while (nums2[i] > stack[stack.length - 1] && stack.length) {
+        while (stack.length) {
+            if (stack[stack.length - 1] < nums2[i]) {
                 stack.pop()
+            } else {
+                map[nums2[i]] = stack[stack.length - 1]
+                break;
             }
-
-            let isTop = stack[stack.length - 1] ? stack[stack.length - 1] : -1
-            map[nums2[i]] = isTop;
-            stack.push(nums2[i])
         }
+        if (stack.length === 0) {
+            map[nums2[i]] = -1
+        }
+        stack.push(nums2[i])
         --i
     }
 
-    console.log(map)
-
-    let ans = []
-    let j = 0;
-    while (j < nums1.length) {
-        ans.push(map[nums1[j]])
-        ++j
-    }
-
-    return ans
+    return nums1.map(x=> map[x])
 };
+
+

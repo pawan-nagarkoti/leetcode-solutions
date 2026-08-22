81863f7bd15e028b60a506e476dc52c7177aef37
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-22 08:47:10 +0530

    Time: 21 ms (18.43%) | Memory: 60 MB (19.98%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+    let arth = ["+", "-", "*", "/"]
+
+    let i = 0;
+    let stack = []
+    while (i < tokens.length) {
+        if (!arth.includes(tokens[i])) {
+            stack.push(tokens[i])
+        } else {
+            let firstElm = stack.pop()
+            let secondElm = stack.pop()
+            let c = eval(``)
+            let ans = Math.trunc(eval(`${secondElm}  ${tokens[i]}  ${firstElm}`))
+            stack.push(ans)
+        }
+        ++i
+    }
+    return Number(stack[stack.length-1])
+};

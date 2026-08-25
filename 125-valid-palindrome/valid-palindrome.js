e7950e50623e4a60b0136723df5eb643cf55be8a
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-25 19:35:02 +0530

    Time: 10 ms (16.94%) | Memory: 65.5 MB (5.12%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -2,79 +2,17 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function (s) {
-    const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-    const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-    const number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-    const allLetters = [...lowercase, ...uppercase];
-
-    let str = '';
-    let i = 0;
-
-    while (i < s.length) {
-        console.log(typeof s[i])
-        if (allLetters.includes(s[i])) {
-            str += s[i].toLowerCase()
+var isPalindrome = function(s) {
+    s=s.toLowerCase()
+    let st = ''
+    let rev = '';
+    let i=s.length-1;
+    while(i>=0){
+        if(s[i].match(/[a-z0-9]/i)){
+            st=s[i] + st;
+            rev+=s[i]
         }
-        else if (number.includes(s[i])) {
-            str += s[i]
-        }
-        ++i
+        --i
     }
-
-    let x = 0;
-    let k = str.length - 1;
-    console.log(str)
-    while (x < k) {
-        if (str[x] === str[k]) {
-            ++x;
-            --k
-        } else {
-            return false;
-        }
-    }
-    return true
+    return st === rev
 };
-
-
-
-var isPalindrome = function (s) {
-    let str = '';
-    let i = 0;
-    let rev = ''
-    while (i < s.length) {
-        if (s[i].match(/[a-z0-9]/i)) {
-            str += s[i].toLowerCase();
-            rev = s[i].toLowerCase() + rev;
-        }
-
-        ++i
-    }
-
-    return str === rev
-}
-
-
-var isPalindrome = function (s) {
-    s = s.toLowerCase();
-    let str = '';
-    let i = 0;
-    let rev = ''
-    while (i < s.length) {
-        if (s[i].charCodeAt() >= 'a'.charCodeAt() &&
-            s[i].charCodeAt() <= 'z'.charCodeAt() ||
-            s[i].charCodeAt() >= '0'.charCodeAt() &&
-            s[i].charCodeAt() <= '9'.charCodeAt()) {
-            str += s[i];
-            rev = s[i] + rev
-        }
-        ++i
-    }
-
-    return str===rev
-}

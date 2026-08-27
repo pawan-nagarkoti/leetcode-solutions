d0f4388992bd2c5315282b793bce466b38ff70b2
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-27 19:23:47 +0530

    Time: 4 ms (34.25%) | Memory: 60.8 MB (88.19%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.js
+++ 2-add-two-numbers/add-two-numbers.js
@@ -42,3 +42,32 @@ var addTwoNumbers = function (l1, l2) {
 
     return sentinel.next;
 };
+
+
+var addTwoNumbers = function (l1, l2) {
+    let sentinal = new ListNode()
+    let prev = sentinal
+    let carry = 0
+    while (l1 || l2) {
+        let a = l1 ? l1.val : 0;
+        let b = l2 ? l2.val : 0;
+
+        let ans = a + b + carry;
+        carry = Math.floor(ans / 10);
+        let digit = ans % 10;
+
+        prev.next = new ListNode(digit)
+        prev = prev.next;
+
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next
+
+    }
+
+    if (carry) {
+        prev.next = new ListNode(carry);
+        prev = prev.next
+    }
+    return sentinal.next
+
+}

20f5760574abf751ee9fca67d1cd7a74a3a9498f
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-08-29 19:38:13 +0530

    Time: 0 ms (100.00%) | Memory: 57.1 MB (5.17%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -3,16 +3,13 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
+var search = function (nums, target) {
     let map = {}
-    let i=0;
-    while(i<nums.length){
+    let i = 0;
+    while (i < nums.length) {
         map[nums[i]] = i
         ++i
     }
 
-    console.log(map)
-    console.log(map[target])
-
-    return map[target] || map[target]==0 ? map[target] : -1
+    return map[target] || map[target] == 0 ? map[target] : -1
 };

9ba0aee09701d7be6e78327e75a5f492f76b73df
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-09-03 19:54:11 +0530

    Time: 26 ms (79.64%) | Memory: 67.5 MB (60.44%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -18,9 +18,35 @@ var groupAnagrams = function (strs) {
         ++i
     }
 
-    for(let key in map){
+    for (let key in map) {
         result.push(map[key])
     }
 
     return result
+}
+
+var groupAnagrams = function (strs) {
+    let i = 0
+    let s = []
+    while (i < strs.length) {
+        s.push(strs[i].split('').sort().join(''))
+        ++i
+    }
+
+    let map = {}
+    let j=0
+    while(j<s.length){
+        if(!map[s[j]]){
+            map[s[j]] = [strs[j]]
+        }else{
+            map[s[j]].push(strs[j])
+        }
+        ++j
+    }
+
+    let ans = []
+    for(let key in map){
+        ans.push(map[key])
+    }
+    return ans
 }

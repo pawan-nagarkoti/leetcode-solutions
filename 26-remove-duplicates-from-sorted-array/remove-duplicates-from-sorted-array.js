5a617ac7ac31c14c630061938dadce7a339b75ec
Pawan singh <123574377+pawan-nagarkoti@users.noreply.github.com>
2026-09-09 21:06:15 +0530

    Time: 0 ms (100.00%) | Memory: 58.3 MB (38.33%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let x = 0;
+    let i = 1;
+
+    while (i < nums.length) {
+        if (nums[x] !== nums[i]) {
+            ++x;
+            nums[x] = nums[i]
+        }
+        ++i
+    }
+
+    // nums.length = x +1
+
+    return x+1;
+};
+
+var removeDuplicates = function (nums) {
+    let x=0;
+    let i=0;
+    while(i<nums.length){
+        if(nums[x] !== nums[i]){
+            nums[x+1] = nums[i]
+            ++x
+        }
+        ++i
+    }
+    return nums.length=++x
+}
